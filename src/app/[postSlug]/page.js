7fbf9de471b6b23a32d3dc070c4688654e2ab44f
domzic <domas.zicevicius@gmail.com>
2023-09-20 22:25:08 +0300

    Code snippets with bright

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -4,6 +4,7 @@ import BlogHero from "@/components/BlogHero";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
+import CodeSnippet from "@/components/CodeSnippet";
 
 const cachedLoadBlogPost = React.cache(loadBlogPost);
 
@@ -26,7 +27,7 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={{ CodeSnippet }} />
       </div>
     </article>
   );
