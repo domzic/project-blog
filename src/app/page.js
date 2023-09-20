21da89264eab2613297144314a42e6379ffb231a
domzic <domas.zicevicius@gmail.com>
2023-09-20 22:03:42 +0300

    Display mdx on blog page

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import { getBlogPostList } from "@/helpers/file-helpers";
 
 async function Home() {
   const posts = await getBlogPostList();
-  console.log(posts);
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>Latest Content:</h1>
